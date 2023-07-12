5c957198ef05bf04680ebb279f9a0d76fac4beed
kimberly soler <kimberlysolja@gmail.com>
2023-07-12 11:57:00 -0500

    Se ajusta el main y se adicionan impresiones para mostrar en consola

--- RepasoEjercicios/src/code/kimberly/tiposDeDatos/MainTiposDeDatos.java
+++ RepasoEjercicios/src/code/kimberly/tiposDeDatos/MainTiposDeDatos.java
@@ -1,5 +1,7 @@
 package code.kimberly.tiposDeDatos;
 
+import code.objetos.constructors.Circle;
+
 //import java.time.LocalDate;
 
 import kimberly.code.programacionBasica.Variables;
@@ -13,27 +15,42 @@ public class MainTiposDeDatos {
 		//int n = 1; // es un tipo de dato. Los tipos de datos ayudan a que el código sea más fácil de entender, más organizados y 
 		//tengan menos errores
 		// Class es el mecanismo por el cual permite crear los tipos de datos
+//		
+//		var p1= new punto(10.0, 0.0); //new significa que se va a llamar en este caso el constructor
+//		var p2= new punto(1.0, 1.0); //new significa que se va a llamar en este caso el constructor
+//		
+//		System.out.println(p1.x());		
+//		p1.x();
+//		p2.x();
+//		
+//		System.out.println(punto.distance(p1,p2));		
+//
+//
+//		System.out.println(p1.distance1(p2));
+//		System.out.println(p1.toString());
+//		System.out.println(p2.toString());
+//		
+//		var a = new punto(3.0, 4.5);
+//		var b = new punto(3.0, 4.5);
+//		var c = a;
+//		
+//		System.out.println(a.equals(b)); 
+//		System.out.println(a == c); // el igual igual en objetos es complejo ya que a pesar de que cada objeto tenga la misma información, al ser un objeto diferente no es lo mismo. 
+//		System.out.println(c == b);
+	
+//		var circle1 = new Circle (new Point (0.0, 0.0), 1.0);
+//		System.out.println(circle1.area());
+//		System.out.println(circle1.circumference());
+//		
+//		var circle2 = new Circle (new Point (1.0, 2.0), 3.0);
+//		var circle3 = new Circle (new Point (1.0, 2.0), 3.0);
+//		System.out.println(circle2.intersects(circle3));
 		
-		var p1= new punto(0.0, 0.0); //new significa que se va a llamar en este caso el constructor
-		var p2= new punto(1.0, 1.0); //new significa que se va a llamar en este caso el constructor
 		
-		System.out.println(p1.x());		
-		p1.x();
-		p2.x();
-		
-		System.out.println(punto.distance(p1,p2));		
-		System.out.println(p1.sharedCoordinate(p2));
-		System.out.println(p1.distance1(p2));
-		System.out.println(p1.toString());
-		System.out.println(p2.toString());
-		
-		var a = new punto(3.0, 4.5);
-		var b = new punto(3.0, 4.5);
-		var c = a;
-		
-		System.out.println(a.equals(b)); 
-		System.out.println(a == c); // el igual igual en objetos es complejo ya que a pesar de que cada objeto tenga la misma información, al ser un objeto diferente no es lo mismo. 
-		System.out.println(c == b);
+		var circle4 = new Circle (new Point (0.0, 0.0), 1.0);
+		var circle5 = new Circle (new Point (1.0, 1.0), 1.0);
+		System.out.println(circle4.intersects(circle5));
+	
 	}
 
 }
