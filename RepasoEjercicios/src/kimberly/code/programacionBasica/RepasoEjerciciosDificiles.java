e3f5bab331b64d81b91d220348faf88fe746367e
kimberly soler <kimberlysolja@gmail.com>
2023-08-01 08:32:52 -0500

    Se realiza modificación ejercicio time to seconds acorde a las recomendaciones dadas

--- RepasoEjercicios/src/kimberly/code/programacionBasica/RepasoEjerciciosDificiles.java
+++ RepasoEjercicios/src/kimberly/code/programacionBasica/RepasoEjerciciosDificiles.java
@@ -60,4 +60,45 @@ public class RepasoEjerciciosDificiles {
 		return builder.toString();
 	}
 	
+	private static final int SECONDS_IN_A_MINUTE = 60;
+	private static final int MINUTES_IN_A_HOUR = 60;
+	private static final int SECONDS_IN_A_HOUR = SECONDS_IN_A_MINUTE * MINUTES_IN_A_HOUR;
+	
+	public static int timeToSeconds (String date) {
+		var foundT = date.indexOf("T");
+			var time = foundT != -1 ? date.substring(foundT + 1) : date;
+			var firstPoint = time.indexOf(":");
+			var secondPoint = time.indexOf(":", firstPoint + 1);
+			var hour = Integer.parseInt(time.substring(0, firstPoint));
+			var minutes = Integer.parseInt(time.substring(firstPoint + 1, secondPoint));
+			var seconds = Integer.parseInt(time.substring(secondPoint + 1));
+		return  hour*SECONDS_IN_A_HOUR + minutes*MINUTES_IN_A_HOUR + seconds;
+	}
+	
+	
+	public static String format(String format, String phone) {
+		var builder = new StringBuilder();
+		var indexPhone = 0;
+		for (var i= 0; i < format.length(); i++) {
+			var charFormat = format.charAt(i);
+			if(charFormat != 'X') {
+				builder.append(charFormat);
+			}else {
+				var charPhone = phone.charAt(indexPhone);
+				builder.append(charPhone);
+				indexPhone++;
+			}
+		}
+		return builder.toString();	
+	}
+	
+	
+	public static String formattedPhone(String phone) {
+		return switch (phone.length()){
+		case 7 -> format("XXX-XXXX", phone);
+		case 10 -> format("(XXX) XXX-XXXX", phone);
+		default -> phone;
+		};
+	}
+	
 }
